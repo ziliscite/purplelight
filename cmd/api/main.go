0d260ac6a748a6fa021ff93abc5bd0946599b039
Zil <Manzil.akbar@gmail.com>
2025-01-11 21:08:42 +0700

    First commit

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+// Declare a string containing the application version number. Later in the book we'll
+// generate this automatically at build time, but for now we'll just store the version
+// number as a hard-coded global constant.
+const version = "1.0.0"
+
+// Define a config struct to hold all the configuration settings for our application.
+// For now, the only configuration settings will be the network port that we want the
+// server to listen on, and the name of the current operating env for the
+// application (development, staging, production, etc.). We will read in these
+// configuration settings from command-line flags when the application starts.
+type config struct {
+	port int
+	env  string
+}
+
 func main() {
 	fmt.Println("Hello world!")
 }
